e445163c0c131bc4d17e39010a18ee668d8ffd5d
unknown <yeungson@gmail.com>
2025-08-21 19:27:22 +0100

    trigger deployment

--- function_app.py
+++ function_app.py
@@ -9,6 +9,7 @@ from azure.core.exceptions import ResourceNotFoundError
 
 app = func.FunctionApp(http_auth_level=AuthLevel.ANONYMOUS)
 
+#trigger a new deployment
 
 PK_TOTAL = "counter"
 RK_TOTAL = "visits"
